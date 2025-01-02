d0561aa8229eb20c873c0ae35ffa90b33a207f53
Exe Soler <exesoler@gmail.com>
2025-01-02 14:32:28 -0300

    Marco Curricular v1.0 [+] PDF.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,8 @@ import {footer} from './footer.js';
 document.querySelector('header').innerHTML = header();
 document.querySelector('footer').innerHTML = footer();
 
+setTimeout(() => {
+    PDFObject.embed("/src/assets/prueba.pdf", "#pdfContainer");
+}, 2000);
+
 // document.querySelector('main').innerHTML = ``
