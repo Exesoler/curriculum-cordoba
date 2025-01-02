94fdf8d7d272bb25b83909dbca1c0b411e6eedfe
Exe Soler <exesoler@gmail.com>
2025-01-02 12:12:54 -0300

    Home v1.0

---
+++ src/js/footer.js
@@ -0,0 +1,18 @@
+export const header = () => {
+    return `
+<div class="logoContainer">
+    <picture>
+        <source srcset='/src/assets/logoH.svg' type='image/svg+xml'>
+        <source srcset='/src/assets/logoH.webp' type='image/webp'>
+        <img src='/src/assets/logoH.png' type='image/png' alt='Logo de Currículum Córdoba'>
+    </picture>
+</div>
+<nav>
+    <ul>
+        <li><a href="#marcoCurricular"><img src="/src/assets/asideIcon__marcoCurricular.svg" type='image/svg+xml' alt="Marco Curricular Común">Marco Curricular Común</a></li>
+        <li><a href="#marcoCurricular"><img src="/src/assets/asideIcon__progresionAprendizajes.svg" type='image/svg+xml' alt="Marco Curricular Común">Progresión
+            de aprendizajes</a></li>
+        <li><a href="#marcoCurricular"><img src="/src/assets/asideIcon__metas.svg" type='image/svg+xml' alt="Marco Curricular Común">Metas 2025</a></li>
+    </ul>
+</nav>
+`;}
