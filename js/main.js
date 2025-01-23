a1b978e46e427eb64d084fa8a5980de51483a62e
Exequiel Soler <exesoler@gmail.com>
2025-01-23 17:15:19 -0300

    [+] v0.1 libreria pdf.js - Falta estilar

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ import {header} from './header.js';
 import {footer} from './footer.js';
 import {mobileHeader} from './mobileHeader.js';
 
+import { GlobalWorkerOptions } from 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.5.136/build/pdf.min.mjs';
+GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.5.136/build/pdf.worker.min.mjs';
+
+
 document.querySelector('header').innerHTML = header();
 document.querySelector('footer').innerHTML = footer();
 document.querySelector('main').insertAdjacentHTML("afterbegin", mobileHeader());
@@ -51,3 +55,109 @@ if(window.innerWidth < 768){
 }
 
 window.scrollY = 0;
+
+
+const pdf = './../public/downloads/METAS_2025_v1.0.pdf';
+
+const pageNum = document.querySelector('#page_num');
+const pageCount = document.querySelector('#page_count');
+const currentPage = document.querySelector('#current_page');
+const previousPage = document.querySelector('#prev_page');
+const nextPage = document.querySelector('#next_page');
+const zoomIn = document.querySelector('#zoom_in');
+const zoomOut = document.querySelector('#zoom_out');
+
+const initialState = {
+	pdfDoc: null,
+	currentPage: 1,
+	pageCount: 0,
+	zoom: 1,
+};
+
+pdfjsLib
+.getDocument(pdf)
+.promise.then((data) => {
+    initialState.pdfDoc = data;
+    console.log('pdfDocument', initialState.pdfDoc);
+
+    pageCount.textContent = initialState.pdfDoc.numPages;
+
+    renderPage();
+})
+.catch((err) => {
+    alert(err.message);
+});
+
+// Render the page.
+const renderPage = () => {
+	// Load the first page.
+	console.log(initialState.pdfDoc, 'pdfDoc');
+	initialState.pdfDoc
+		.getPage(initialState.currentPage)
+		.then((page) => {
+			console.log('page', page);
+
+			const canvas = document.querySelector('#pdf-canvas');
+			const ctx = canvas.getContext('2d');
+			const viewport = page.getViewport({
+				scale: initialState.zoom,
+			});
+
+			canvas.height = viewport.height;
+			canvas.width = viewport.width;
+
+			// Render the PDF page into the canvas context.
+			const renderCtx = {
+				canvasContext: ctx,
+				viewport: viewport,
+			};
+
+			page.render(renderCtx);
+
+			pageNum.textContent = initialState.currentPage;
+		});
+};
+
+const showPrevPage = () => {
+	if (initialState.pdfDoc === null || initialState.currentPage <= 1)
+		return;
+	initialState.currentPage--;
+	// Render the current page.
+	currentPage.value = initialState.currentPage;
+	renderPage();
+};
+
+const showNextPage = () => {
+	if (
+		initialState.pdfDoc === null ||
+		initialState.currentPage >= initialState.pdfDoc._pdfInfo.numPages
+	)
+		return;
+
+	initialState.currentPage++;
+	currentPage.value = initialState.currentPage;
+	renderPage();
+};
+
+// Button events.
+previousPage.addEventListener('click', showPrevPage);
+nextPage.addEventListener('click', showNextPage);
+
+// Keypress event.
+currentPage.addEventListener('keypress', (event) => {
+	if (initialState.pdfDoc === null) return;
+	// Get the key code.
+	const keycode = event.keyCode ? event.keyCode : event.which;
+
+	if (keycode === 13) {
+		// Get the new page number and render it.
+		let desiredPage = currentPage.valueAsNumber;
+		initialState.currentPage = Math.min(
+			Math.max(desiredPage, 1),
+			initialState.pdfDoc._pdfInfo.numPages,
+		);
+
+		currentPage.value = initialState.currentPage;
+		renderPage();
+	}
+});
