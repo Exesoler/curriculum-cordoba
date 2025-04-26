2a275c89450cc99c2771e1c6c236d3545aa7365e
Exequiel Soler <exesoler@gmail.com>
2025-04-26 20:10:24 -0300

    [v2.0] Progresiones Lengua y Matematica [+] Progresiones Sociales y Naturales [v2.0] Progresiones de aprendizaje

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ module.exports = defineConfig({
         index: './index.html',
         quienes_somos: './quienes_somos.html',
         progresiones_aprendizaje: './progresiones_aprendizaje.html',
+        progresiones_lengua_matematica: './progresiones_lengua_matematica.html',
+        progresiones_sociales_naturales: './progresiones_sociales_naturales.html',
         metas_ciclo: './metas_ciclo.html',
         marco_curricular: './marco_curricular.html',
         contacto: './contacto.html',
